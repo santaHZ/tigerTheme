8c182e971e53943c931f467c5327cf2132078b18
santaHZ <nathan@santahz.com>
2022-01-07 09:12:09 +0800

    20220107am0911(adding fontawesome star)

--- assets/main-index.js
+++ assets/main-index.js
@@ -16,3 +16,18 @@ imageImg.forEach((imageImgchild,index)=> {
         }, 300);
     })
 })
+
+var imagePopularEBikes = document.querySelectorAll('.popularEBikes > ul > a > img');
+
+imagePopularEBikes.forEach((imageImgchild,index)=> {
+    imagePopularEBikes[index].addEventListener('mouseover', function(){
+        imagePopularEBikes[index].classList.remove('zoomImgOut');
+        imagePopularEBikes[index].classList.add('zoomImgIn');
+        // console.log('mouse over');
+        setTimeout(() => {
+            // console.log('mouse out');
+            imagePopularEBikes[index].classList.remove('zoomImgIn');
+            imagePopularEBikes[index].classList.add('zoomImgOut');
+        }, 300);
+    })
+})
