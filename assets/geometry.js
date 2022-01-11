67757714236a51d7df97f10aeb3f92c8da90a21a
santaHZ <nathan@santahz.com>
2022-01-11 19:54:00 +0800

    20220111pm0753(finishing product template to camel-men total 2 product finished)

--- assets/geometry.js
+++ assets/geometry.js
@@ -1,21 +1,26 @@
 // This is tabBtn JavaScript code!
 console.log("start");
 
+//******************Define tab button img url*************************//
+// const tabBtnOverall = 'https://cdn.shopify.com/s/files/1/0614/0490/5716/files/overall01.jpg?v=1638937485';
+// const tabBtnFolded = 'https://cdn.shopify.com/s/files/1/0614/0490/5716/files/folded01.jpg?v=1638937485';
+
 const tabDivOverall = document.querySelector("#geometry .tabInfo .overall");
 const tabDivFolded = document.querySelector("#geometry .tabInfo .folded");
 const tabBtn1 = document.querySelector("#geometry .tabBtnDiv .tabBtn1");
 const tabBtn2 = document.querySelector("#geometry .tabBtnDiv .tabBtn2");
 
-const DivOverallWrapper = tabDivOverall.firstElementChild;
-const DivFoldedWrapper = tabDivFolded.firstElementChild;
+// const DivOverallWrapper = tabDivOverall.firstElementChild;
+// const DivFoldedWrapper = tabDivFolded.firstElementChild;
 //let tabDivFoldedWrapperData = document.querySelector("#geometry .tabInfo .folded .foldedWrapper .foldedlWrapperData");
 // console.log(DivOverallWrapper);
 
-const overallImg = DivOverallWrapper.firstElementChild.firstElementChild;
+// setting the image of the tab
+/* const overallImg = DivOverallWrapper.firstElementChild.firstElementChild;
 overallImg.setAttribute('src', tabBtnOverall);
 
 const foldedImg = DivFoldedWrapper.firstElementChild.firstElementChild;
-foldedImg.setAttribute('src', tabBtnFolded);
+foldedImg.setAttribute('src', tabBtnFolded); */
 
 // console.log(overallImg);
 
